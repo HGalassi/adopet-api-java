73fd643212625c67829e5354a457f2d1ceccc6fb
unknown <hgalassi17@gmail.com>
2021-07-10 15:27:25 -0300

    Adding new Unit Tests

--- adopet-api-java/src/main/java/entities/User.java
+++ adopet-api-java/src/main/java/entities/User.java
@@ -7,11 +7,17 @@ public class User {
     private final int age;
     private final String address;
 
-    public User(String name, String lastName, int age, String address) {
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
-        this.address = address;
+    public User(String name, String lastName, int age, String address) throws Exception {
+
+        if(hasValidAge(age)){
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+            this.address = address;
+        }else{
+            throw new Exception(" deu merda");
+        }
+
     }
 
     public String getFullName(String name, String lastName) {
@@ -20,5 +26,15 @@ public class User {
 
     public void addFullName(String name, String lastName) {
         name.concat(" ".concat(lastName));
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean hasValidAge(int age) throws Exception {
+        if (age < 18)
+            throw new Exception("deu ruim");
+        return true;
     }
 }
--- adopet-api-java/src/test/java/entities/UserTest.java
+++ adopet-api-java/src/test/java/entities/UserTest.java
@@ -2,11 +2,18 @@ package entities;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+
+import static com.googlecode.catchexception.apis.BDDCatchException.caughtException;
+import static com.googlecode.catchexception.apis.BDDCatchException.when;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.BDDMockito.given;
 
 public class UserTest {
 
     @Test
-    public void shouldReturnFullName(){
+    public void shouldReturnFullName() throws Exception {
         String name = "a Name";
         String lastName = "a Last Name";
         int age = 25;
@@ -16,7 +23,7 @@ public class UserTest {
     }
 
     @Test
-    public void shouldAddValidFullName(){
+    public void shouldAddValidFullName() throws Exception {
         String name = "a Name";
         String lastName = "a Last Name";
         int age = 25;
@@ -26,4 +33,27 @@ public class UserTest {
         Assertions.assertTrue(user.getFullName(name,lastName).equals(name.concat(" ".concat(lastName))));
     }
 
+    @Test
+    public void shouldReturnOnlyFirstName() throws Exception {
+        String name = "a Name";
+        String lastName = "a Last Name";
+        int age = 25;
+        String address = " Some Address Here";
+        User user = new User(name,lastName,age,address);
+
+        Assertions.assertTrue(user.getName().equals("a Name"));
+    }
+
+
+    //https://gist.github.com/mariuszs/7489190 sample
+    @Test
+    public void shouldReturnValidatedAge() throws Exception {
+        String name = "a Name";
+        String lastName = "a Last Name";
+        int age = 15;
+        String address = " Some Address Here";
+
+        when(() -> new User(name,lastName,age,address));
+        then(caughtException()).isInstanceOf(Exception.class).hasMessage("deu ruim");
+    }
 }
